e68e6131288488866fc76063e021d9272943b067
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-01-20 12:56:10 +0100

    Programa simple sobre un listado de asistentes y su registro

--- src/Asistente.java
+++ src/Asistente.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class Asistente {
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    public Asistente(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Asistente asistente = (Asistente) o;
+        return Objects.equals(nombre, asistente.nombre) && Objects.equals(apellido, asistente.apellido) && Objects.equals(email, asistente.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, apellido, email);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,66 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Set<Asistente> asistentes = new HashSet<>();
+
+        boolean continuar = true;
+        while (continuar) {
+            //Menu, con opciones sobre el HashSet
+            System.out.println("MENU");
+            System.out.println("1. Registrar asistente");
+            System.out.println("2. Buscar asistente (email)");
+            System.out.println("3. Mostrar numero de asistentes");
+            System.out.println("4. Mostrar listado de asistentes (email)");
+            System.out.println("0. Salir");
+            System.out.println("Ingrese opcion:");
+            int opcion = sc.nextInt();
+            sc.nextLine();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ingrese asistentes que desea registrar: (Nombre, Apellido, Email)");
+                    System.out.println("ingrese su nombre:");
+                    String nombre = sc.nextLine();
+                    System.out.println("Ingrese su apellido:");
+                    String apellido = sc.nextLine();
+                    System.out.println("Ingrese su email:");
+                    String email = sc.nextLine();
+                    Asistente asistente = new Asistente(nombre, apellido, email);
+                    asistentes.add(asistente);
+                    break;
+                case 2:
+                    System.out.println("Ingrese email a buscar: ");
+                    String emailBuscar = sc.nextLine();
+                    for(Asistente a : asistentes) {
+                        if(a.getEmail().equals(emailBuscar)) {
+                            System.out.println("Asistente encontrado");
+                        }else  {
+                            System.out.println("Asistente no encontrado");
+                        }
+                    }
+                    break;
+                case 3:
+                    asistentes.size();
+                    break;
+                case 4:
+                    System.out.println("Listado de asistentes: ");
+                    for(Asistente a : asistentes) {
+                        System.out.println(a.getEmail());
+                    }
+                    break;
+                case 0:
+                    System.out.println("Saliendo...");
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta");
+                    break;
+            }
+
+        }
+        sc.close();
+    }
 }
